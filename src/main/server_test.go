6d7175e054a4c3e991b82526067f93cac987ade3
Arnau Canyadell <arnau.canyadell@est.fib.upc.edu>
2020-08-01 15:54:24 +0200

    F1C. Cleaned tests

--- src/main/server_test.go
+++ src/main/server_test.go
@@ -15,15 +15,63 @@ type testOptions struct {
 	url  string
 }
 
-func RunTest(t *testing.T, s *UsersServer, op *testOptions) {
-	request, _ := http.NewRequest(http.MethodGet, op.url, nil)
+func RunGetUsersTest(t *testing.T, s *UsersServer, name, want string) {
+	request, _ := http.NewRequest(http.MethodGet, "/getUsers", nil)
 	response := httptest.NewRecorder()
 
 	s.ServeHTTP(response, request)
 
-	t.Run(op.name, func(t *testing.T) {
+	t.Run(name, func(t *testing.T) {
 		got := response.Body.String()
-		want := op.want
+		want := want
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func RunTest(t *testing.T, s *UsersServer, name, url, want string) {
+	request, _ := http.NewRequest(http.MethodGet, url, nil)
+	response := httptest.NewRecorder()
+
+	s.ServeHTTP(response, request)
+
+	t.Run(name, func(t *testing.T) {
+		got := response.Body.String()
+		want := want
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func RunSignUpTest(t *testing.T, s *UsersServer, name, password, want string) {
+	requestBody, err := json.Marshal(map[string]string{
+		"name": name,
+		"pass": password,
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, "/signUp", bytes.NewBuffer(requestBody))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request.Header.Set("Content-type", "application/json")
+
+	response := httptest.NewRecorder()
+
+	s.ServeHTTP(response, request)
+
+	t.Run("sign up a new user", func(t *testing.T) {
+		got := response.Body.String()
+		want := want
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
@@ -41,10 +89,10 @@ func TestGetUsers(t *testing.T) {
 	server := &UsersServer{store: &store}
 
 	// Request users
-	RunTest(t, server, &testOptions{name: "returns list of users in the social network", want: "[arnau]", url: "/getUsers"})
+	RunGetUsersTest(t, server, "returns list of users in the social network", "[arnau]")
 
 	// Wrong request
-	RunTest(t, server, &testOptions{name: "unused url path: should return no string", want: "", url: "/someUnusedPath"})
+	RunTest(t, server, "unused url path: should return no string", "/someUnusedPath", "")
 }
 
 func TestSignUp(t *testing.T) {
@@ -53,39 +101,11 @@ func TestSignUp(t *testing.T) {
 	server := &UsersServer{store: store}
 
 	// Request users
-	RunTest(t, server, &testOptions{name: "list of users at the beginning should be empty", want: "[]", url: "/getUsers"})
+	RunGetUsersTest(t, server, "list of users at the beginning should be empty", "[]")
 
 	// Sign up new user
-	requestBody, err := json.Marshal(map[string]string{
-		"name": "arnau",
-		"pass": "1234",
-	})
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	request, err := http.NewRequest(http.MethodPost, "/signUp", bytes.NewBuffer(requestBody))
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	request.Header.Set("Content-type", "application/json")
-
-	response := httptest.NewRecorder()
-
-	server.ServeHTTP(response, request)
-
-	t.Run("sign up a new user", func(t *testing.T) {
-		got := response.Body.String()
-		want := "ok"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	RunSignUpTest(t, server, "arnau", "1234", "ok")
 
 	// Request users
-	RunTest(t, server, &testOptions{name: "list of users should include recently created user", want: "[arnau]", url: "/getUsers"})
+	RunGetUsersTest(t, server, "list of users should include recently created user", "[arnau]")
 }
