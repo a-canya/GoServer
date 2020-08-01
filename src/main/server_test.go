93021de00adfc951efb254cbc8e7dc5daab7f879
Arnau Canyadell <arnau.canyadell@est.fib.upc.edu>
2020-08-01 12:25:00 +0200

    F1A. Create user test

--- src/main/server_test.go
+++ src/main/server_test.go
@@ -1,40 +1,79 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestGetUsers(t *testing.T) {
-	// Request users
-	request, _ := http.NewRequest(http.MethodGet, "/getUsers", nil)
+type testOptions struct {
+	name string
+	want string
+	url  string
+}
+
+func RunTest(t *testing.T, op *testOptions) {
+	request, _ := http.NewRequest(http.MethodGet, op.url, nil)
 	response := httptest.NewRecorder()
 
 	Server(response, request)
 
-	t.Run("returns list fo users in the social network", func(t *testing.T) {
+	t.Run(op.name, func(t *testing.T) {
 		got := response.Body.String()
-		want := "[arnau]"
+		want := op.want
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
+}
+
+func TestGetUsers(t *testing.T) {
+	// Request users
+	RunTest(t, &testOptions{name: "returns list of users in the social network", want: "[arnau]", url: "/getUsers"})
 
 	// Wrong request
-	request2, _ := http.NewRequest(http.MethodGet, "/someUnusedPath", nil)
-	response2 := httptest.NewRecorder()
+	RunTest(t, &testOptions{name: "unused url path: should return no string", want: "", url: "/someUnusedPath"})
+}
 
-	Server(response2, request2)
+func TestSignUp(t *testing.T) {
+	// Request users
+	RunTest(t, &testOptions{name: "list of users at the beginning should be empty", want: "[]", url: "/getUsers"})
 
-	t.Run("unused url path: returns error", func(t *testing.T) {
-		got := response2.Body.String()
-		want := ""
+	// Sign up new user
+	requestBody, err := json.Marshal(map[string]string{
+		"name": "arnau",
+		"pass": "1234",
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, "/signUp", bytes.NewBuffer(requestBody))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request.Header.Set("Content-type", "application/json")
+
+	response := httptest.NewRecorder()
+
+	Server(response, request)
+
+	t.Run("sign up a new user", func(t *testing.T) {
+		got := response.Body.String()
+		want := "ok"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
 
+	// Request users
+	RunTest(t, &testOptions{name: "list of users should include recently created user", want: "[arnau]", url: "/getUsers"})
 }
