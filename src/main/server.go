060c49c7b2c95580dd439788b5b5aa60949b29f2
Arnau Canyadell <arnau.canyadell@est.fib.upc.edu>
2020-08-01 17:54:56 +0200

    F13B. code to check usernames/passwords

--- src/main/server.go
+++ src/main/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -23,8 +24,10 @@ type UsersServer struct {
 func (s *UsersServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	option := strings.Split(r.URL.Path, "/")[1]
 	switch option {
+
 	case "getUsers":
 		fmt.Fprint(w, s.store.GetUsers())
+
 	case "signUp":
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
@@ -35,6 +38,10 @@ func (s *UsersServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		var info map[string]string
 		json.Unmarshal(body, &info)
 
+		if ok := CheckUsernameAndPassword(info["name"], info["pass"], &w); !ok {
+			return
+		}
+
 		if ok := s.store.AddUser(info["name"], info["pass"]); ok {
 			w.WriteHeader(http.StatusOK)
 		} else {
@@ -42,4 +49,48 @@ func (s *UsersServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "User already exists")
 		}
 	}
+}
+
+// CheckUsernameAndPassword returns true iff username has 5-10 alphanum characters and password has 8-12 alphanum chars.
+// If conditions are not fulfilled, prints an error message in w
+func CheckUsernameAndPassword(username, password string, w *http.ResponseWriter) bool {
+	ok := true
+
+	var isStringAlphabetic = regexp.MustCompile(`^[a-zA-Z0-9_]*$`).MatchString
+
+	if !isStringAlphabetic(username) {
+		(*w).WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(*w, "Username has invalid characters! Username must be unique, from 5 to 10 alphanumeric characters.")
+		ok = false
+	}
+
+	if !isStringAlphabetic(password) {
+		(*w).WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(*w, "Password has invalid characters! Password must have from 8 to 12 alphanumeric characters.")
+		ok = false
+	}
+
+	// Note: checking len(var) returns the length in bytes but this might not correspond to the number of characters because
+	// standard allows characters of multiple bytes. However, since we only accept alphanumeric characters this is ok
+	if len(username) < 5 {
+		(*w).WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(*w, "Username too short! Username must be unique, from 5 to 10 alphanumeric characters.")
+		ok = false
+	} else if len(username) > 10 {
+		(*w).WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(*w, "Username too long! Username must be unique, from 5 to 10 alphanumeric characters.")
+		ok = false
+	}
+
+	if len(password) < 8 {
+		(*w).WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(*w, "Password too short! Password must have from 8 to 12 alphanumeric characters.")
+		ok = false
+	} else if len(password) > 12 {
+		(*w).WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(*w, "Password too long! Password must have from 8 to 12 alphanumeric characters.")
+		ok = false
+	}
+
+	return ok
 }
